974e8ae999a0174292a0299c7c36054fde98b5eb
Hyunsujk <hyunsujk@gmail.com>
2020-04-24 16:45:49 -0500

    added router.get

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -1,4 +1,21 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
+const pool = require("../modules/pool");
+
+router.get("/", (req, res) => {
+  const queryString = `SELECT "movies".id, "movies".title, "movies".poster, "movies".description, "genres".name as "genre" FROM "movies"
+  JOIN "movie_genre" ON "movie_genre".movie_id = "movies".id
+  JOIN "genres" ON "movie_genre".genre_id = "genres".id;`;
+  pool
+    .query(queryString)
+    .then((responseDB) => {
+      res.send(responseDB.rows);
+      console.log(responseDB.rows);
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
+const movieRouter = require("./routes/movie.router");
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("build"));
 
 /** ---------- ROUTES ---------- **/
-
+app.use("/", movieRouter);
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
   console.log("Listening on port: ", port);
