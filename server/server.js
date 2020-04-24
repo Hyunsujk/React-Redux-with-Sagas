cbf53eb664d30787ef36a9943b5b765d36be3f39
Hyunsujk <hyunsujk@gmail.com>
2020-04-24 16:23:29 -0500

    added bodyParser.urlencoded for server to receive value from frontend

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(express.static('build'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("build"));
 
 /** ---------- ROUTES ---------- **/
 
-
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
-    console.log('Listening on port: ', port);
+  console.log("Listening on port: ", port);
 });
