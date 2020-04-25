b1a20449ff5bf6d89065f9439a1759982f3e0f48
Hyunsujk <hyunsujk@gmail.com>
2020-04-25 00:11:33 -0500

    updated queryStrings for get details for the selected movie. test good

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -19,34 +19,34 @@ router.get("/movie", (req, res) => {
     });
 });
 
-router.get("/genre", (req, res) => {
-  const queryString = `SELECT "movies".id, "movies".title, array_agg("genres".name) as "genre" FROM "movies"
-  JOIN "movie_genre" ON "movie_genre".movie_id = "movies".id
-  JOIN "genres" ON "movie_genre".genre_id = "genres".id
-  GROUP BY "movies".id
-  ORDER BY "movies".id ASC;`;
-  pool
-    .query(queryString)
-    .then((responseDB) => {
-      res.send(responseDB.rows);
-    })
-    .catch((err) => {
-      console.log("Error:", err);
-      res.sendStatus(500);
-    });
-});
+// router.get("/genre", (req, res) => {
+//   const queryString = `SELECT "movies".id, "movies".title, array_agg("genres".name) as "genre" FROM "movies"
+//   JOIN "movie_genre" ON "movie_genre".movie_id = "movies".id
+//   JOIN "genres" ON "movie_genre".genre_id = "genres".id
+//   GROUP BY "movies".id
+//   ORDER BY "movies".id ASC;`;
+//   pool
+//     .query(queryString)
+//     .then((responseDB) => {
+//       res.send(responseDB.rows);
+//     })
+//     .catch((err) => {
+//       console.log("Error:", err);
+//       res.sendStatus(500);
+//     });
+// });
 
 router.get("/details/:id", (req, res) => {
   let reqId = req.params.id;
   const queryString = `SELECT "movies".id, "movies".title, "movies".poster, "movies".description, array_agg("genres".name) as "genre" FROM "movies"
-      JOIN "movie_genre" ON "movie_genre".movie_id = "movies".id
-      JOIN "genres" ON "movie_genre".genre_id = "genres".id
-      GROUP BY "movies".id
-      ORDER BY "movies".id ASC
-      WHERE "movies".id =$1;`;
+  JOIN "movie_genre" ON "movie_genre".movie_id = "movies".id
+  JOIN "genres" ON "movie_genre".genre_id = "genres".id
+  WHERE "movies".id =$1
+  GROUP BY "movies".id`;
   pool
     .query(queryString, [reqId])
     .then((responseDB) => {
+      console.log(responseDB.rows);
       res.send(responseDB.rows);
     })
     .catch((err) => {
