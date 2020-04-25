fc90af4b38517006a69ac77ab50157435c00fcf1
Hyunsujk <hyunsujk@gmail.com>
2020-04-25 09:57:20 -0500

    styled title and used grid to display poster and descriptions

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import "./Movie.css";
 
 class Movie extends Component {
   render() {
     return (
       <div>
-        <img
-          onClick={this.props.clickPoster(this.props.id)}
-          src={this.props.poster}
-          alt="poster"
-        />
-        <h3>{this.props.title}</h3>
-        <p>{this.props.description}</p>
+        <h3 className="title">{this.props.title}</h3>
+        <div className="grid-container">
+          <div className="poster-img">
+            <img
+              className="grid-item"
+              onClick={this.props.clickPoster(this.props.id)}
+              src={this.props.poster}
+              alt="poster"
+            />
+          </div>
+          <p className="grid-item description">{this.props.description}</p>
+        </div>
       </div>
     );
   }
