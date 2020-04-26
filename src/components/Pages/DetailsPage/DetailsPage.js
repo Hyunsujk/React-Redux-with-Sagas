0b0c5ec1262cbb3eead38a7c4623999edae14514
Hyunsujk <hyunsujk@gmail.com>
2020-04-26 12:22:56 -0500

    added an icon on edit button

--- src/components/Pages/DetailsPage/DetailsPage.js
+++ src/components/Pages/DetailsPage/DetailsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Button from "@material-ui/core/Button";
 import "./DetailsPage.css";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class DetailsPage extends Component {
@@ -89,7 +90,7 @@ class DetailsPage extends Component {
             margin: "15px 30px",
           }}
         >
-          Edit
+          <FontAwesomeIcon icon={faEdit} /> Edit
         </Button>
       </div>
     );
