e2554f81df7df5f3b043faafb4e574360c9b85fd
Hyunsujk <hyunsujk@gmail.com>
2020-04-25 19:21:35 -0500

    cleaned code and added comments

--- src/components/Pages/DetailsPage/DetailsPage.js
+++ src/components/Pages/DetailsPage/DetailsPage.js
@@ -2,27 +2,34 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Button from "@material-ui/core/Button";
 import "./DetailsPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class DetailsPage extends Component {
+  // when the components are mounted, do GET_LIST
   componentDidMount() {
     this.props.dispatch({ type: "GET_LIST" });
   }
 
+  // take the user back to list page
   clickBackToList = () => {
     this.props.history.push("/");
   };
 
+  // take the user to edit page with the id of the movie
   clickEdit = () => {
     this.props.history.push(`/edit/${this.props.match.params.id}`);
   };
 
   render() {
+    //get id from the url
     const id = Number(this.props.match.params.id);
 
+    // if the id matches, get the movie information from movies reducer
     const selectedMovie = this.props.store.movies.filter((movie) => {
       return movie.id === id;
     });
 
+    // if the id matches, get the genre from the genres reducer
     const genreOfMovie = this.props.store.genres.filter((genre) => {
       return genre.id === id;
     });
@@ -40,6 +47,7 @@ class DetailsPage extends Component {
       return (
         <div key={genre.id}>
           <div className="detail-genre-container">
+            {/* go through the genre and display one by one */}
             {genre.genre.map((genre, index) => {
               return (
                 <div key={index}>
@@ -52,12 +60,10 @@ class DetailsPage extends Component {
       );
     });
 
-    console.log(info);
-
     return (
       <div>
         <header className="App-header">
-          <h1 className="App-title">Movie Details</h1>
+          <h1 className="App-title">MOVIE DETAILS</h1>
         </header>
 
         <div className="detail-desc">{info}</div>
